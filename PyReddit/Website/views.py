74a3a6ae4f7e15b85a15adc31b57ff9ffda7e170
Rishik Mourya <braindotai@gmail.com>
2019-12-13 16:45:20 +0530

    Initial server

--- PyReddit/Website/views.py
+++ PyReddit/Website/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+posts = [
+    {'title': 'Blog 1',
+    'author': 'Rishik',
+    'description': 'In this blog we will cover how to improve performance in loops',
+    'date_posted': 'August 27, 2019'},
+    
+    {'title': 'Blog 2',
+     'author': 'Rudra',
+     'description': 'Comparing inference performance of different deep learning frameworks',
+     'date_posted': 'August 29, 2019'}
+]
+
+def home(request):
+    context = {'posts': posts}
+    return render(request, 'Website/home.html', context = context)
