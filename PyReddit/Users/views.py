f78c45d24463680c58aeda1802dfe7fcacc580da
Rishik Mourya <braindotai@gmail.com>
2019-12-14 23:22:49 +0530

    initiated users app

---
+++ PyReddit/Users/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import login, logout
+from .forms import UserSignUpForm, UserSignInForm
+
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserSignUpForm(data = request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Account is created successfully. You can now sign in!")
+            return redirect('users-signin')
+        else:
+            messages.error(request, f"Error occured while signing up!")
+    else:
+        form = UserSignUpForm()
+    return render(request, 'Users/signup.html', context = {'form': form, 'title': 'Sign Up'})
+    
+
+def signin(request):
+    if request.method == 'POST':
+        form = UserSignInForm(data = request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            messages.success(request, "You are succesfully signed in!")
+            return redirect('website-home')
+        else:
+            messages.error(request, "Error occured while signing in!")
+    else:
+        form = UserSignInForm()
+    return render(request, 'Users/signin.html', context = {'form': form, 'title': 'Sign In'})
+
+
+def signout(request):
+    logout(request)
+    messages.success(request, "You are succesfully signed out!")
+    return redirect('website-home')
