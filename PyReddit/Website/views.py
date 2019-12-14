eaab01dff44d8cf4885216a3e109d03ecd5b29ae
Rishik Mourya <braindotai@gmail.com>
2019-12-14 23:25:23 +0530

    created website home vew

--- PyReddit/Website/views.py
+++ PyReddit/Website/views.py
@@ -1,17 +1,6 @@
 from django.shortcuts import render
-
-posts = [
-    {'title': 'Blog 1',
-    'author': 'Rishik',
-    'description': 'In this blog we will cover how to improve performance in loops',
-    'date_posted': 'August 27, 2019'},
-    
-    {'title': 'Blog 2',
-     'author': 'Rudra',
-     'description': 'Comparing inference performance of different deep learning frameworks',
-     'date_posted': 'August 29, 2019'}
-]
+from .models import Post
 
 def home(request):
-    context = {'posts': posts}
+    context = {'posts': Post.objects.all()}
     return render(request, 'Website/home.html', context = context)
