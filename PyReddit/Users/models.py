8cb661dcb678c13e0ec6fa5678edb0ae64f2af7a
Rishik Mourya <braindotai@gmail.com>
2019-12-15 18:48:58 +0530

    added account model

--- PyReddit/Users/models.py
+++ PyReddit/Users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
 
-# Create your models here.
+class Account(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    image = models.ImageField(default = 'default.png', upload_to = 'profiles')
+
+    def __str__(self):
+        return f"Account for {self.user.username}"
+    
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+        img.thumbnail((300, 300), Image.ANTIALIAS)
+        img.save(self.image.path)
