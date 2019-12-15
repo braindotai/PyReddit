7d2f4b1620c7ebdc2794801e2ccbac7e66c81317
Rishik Mourya <braindotai@gmail.com>
2019-12-15 18:49:55 +0530

    updated urls

--- PyReddit/Users/urls.py
+++ PyReddit/Users/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('signup/', views.signup, name = 'users-signup'),
     path('signin/', views.signin, name = 'users-signin'),
     path('signout/', views.signout, name = 'users-signout'),
+    path('account/', views.account, name = 'users-account')
 ] 
