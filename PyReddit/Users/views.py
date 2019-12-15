0982cce311b746a26d5295b1484b4facd0f0bd95
Rishik Mourya <braindotai@gmail.com>
2019-12-15 18:50:23 +0530

    updated views and added account view

--- PyReddit/Users/views.py
+++ PyReddit/Users/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, logout
-from .forms import UserSignUpForm, UserSignInForm
+from .forms import UserSignUpForm, UserSignInForm, UserUpdateForm, ProfileUpdateForm
 
 
 def signup(request):
@@ -28,7 +29,9 @@ def signin(request):
             user = form.get_user()
             login(request, user)
             messages.success(request, "You are succesfully signed in!")
-            return redirect('website-home')
+            if request.GET.get('next') is None:
+                return redirect('website-home')
+            return redirect(request.GET.get('next'))
         else:
             messages.error(request, "Error occured while signing in!")
     else:
@@ -43,3 +46,26 @@ def signout(request):
     logout(request)
     messages.success(request, "You are succesfully signed out!")
     return redirect('website-home')
+
+
+@login_required
+def account(request):
+    if request.method == 'POST':
+        info_form = UserUpdateForm(request.POST, instance = request.user)
+        profile_form = ProfileUpdateForm(request.POST,
+                                         request.FILES,
+                                         instance = request.user.account)
+        if info_form.is_valid() and profile_form.is_valid():
+            info_form.save()
+            profile_form.save()
+            messages.success(request, "Your account has been updated successfully!")
+            return redirect('users-account')
+    else:
+        info_form = UserUpdateForm(instance = request.user)
+        profile_form = ProfileUpdateForm(instance = request.user.account)
+
+    context = {'title': 'Account',
+               'active': 'account',
+               'info_form': info_form,
+               'profile_form': profile_form}
+    return render(request, 'Users/account.html', context = context) 
