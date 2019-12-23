fc0ef34e18f83b6ab45016f48fcf4b98c45c1f22
Rishik Mourya <braindotai@gmail.com>
2019-12-23 17:56:20 +0530

    created more blog urls

--- PyReddit/Website/urls.py
+++ PyReddit/Website/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
 from . import views
 
 urlpatterns = [
-    path('', views.home, name = 'website-home'),
+    path('', PostListView.as_view(), name = 'website-home'),
+    path('blogs/', PostListView.as_view(), name = 'website-posts'),
+    path('blogs/user/', UserPostListView.as_view(), name = 'website-userposts'),
+    path('blog/<int:pk>/', PostDetailView.as_view(), name = 'website-postdetail'),
+    path('blog/create/', PostCreateView.as_view(), name = 'website-postcreate'),
+    path('blog/update/<int:pk>/', PostUpdateView.as_view(), name = 'website-postupdate'),
+    path('blog/delete/<int:pk>/', PostDeleteView.as_view(), name = 'website-postdelete')
 ]
